21e1045ffe899ca241deaa12b70123065acc7091
test01-hieunnm <hieunguyen7898@gmail.com>
2026-08-01 02:42:29 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,14 @@
+//biến
+let playerName = "Mario";
+let currentLives = "3";
+//hằng
+const level1Coins = 25;
+const level2Coins = 30;
+const level3Coins = 45;
+//sumCoins
+const sumCoins = level1Coins + level2Coins + level3Coins;
+//Trung binh
+const averageCoin = sumCoins / 3;
+//số dư khi chia tổng cho 3
+const remainderCoins = sumCoins % 3;
+console.log(remainderCoins);
